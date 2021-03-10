e13792931f48909542549d03d0afd17203a7486e
nicolas duchamp <nicolas.duchamp60@gmail.com>
2021-03-10 15:59:35 +0100

    working progress

--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
   get 'rides/destroy'
   devise_for :users
   root to: 'pages#home'
+    resources :rides, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
