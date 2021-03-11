f52bec4709922dff0a22a7197a2d2f16b01810cd
Tom Lambot <tom.lambot2502@gmail.com>
2021-03-11 15:42:24 +0100

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,91 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_163503) do
+ActiveRecord::Schema.define(version: 2021_03_10_154751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "booking_start"
+    t.date "booking_end"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "hotel_id", null: false
+    t.bigint "participant_id", null: false
+    t.index ["hotel_id"], name: "index_bookings_on_hotel_id"
+    t.index ["participant_id"], name: "index_bookings_on_participant_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "hotels", force: :cascade do |t|
+    t.string "name"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "hotel_description"
+    t.text "price"
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.bigint "ride_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ride_id"], name: "index_participants_on_ride_id"
+    t.index ["user_id"], name: "index_participants_on_user_id"
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.date "departure_date"
+    t.date "arrival_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "city_departure"
+    t.string "city_arrival"
+    t.string "title"
+    t.string "ride_description"
+    t.float "latitude"
+    t.float "longitude"
+    t.index ["user_id"], name: "index_rides_on_user_id"
+  end
+
+  create_table "steps", force: :cascade do |t|
+    t.integer "order"
+    t.bigint "city_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "ride_id", null: false
+    t.index ["city_id"], name: "index_steps_on_city_id"
+    t.index ["ride_id"], name: "index_steps_on_ride_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +104,18 @@ ActiveRecord::Schema.define(version: 2021_03_09_163503) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "firstname"
+    t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "hotels"
+  add_foreign_key "bookings", "participants"
+  add_foreign_key "participants", "rides"
+  add_foreign_key "participants", "users"
+  add_foreign_key "rides", "users"
+  add_foreign_key "steps", "cities"
+  add_foreign_key "steps", "rides"
 end
