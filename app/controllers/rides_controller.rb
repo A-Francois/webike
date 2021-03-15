7e0e89c744e7d1bebd15b7af5e0c08559ceb7957
nicolas duchamp <nicolas.duchamp60@gmail.com>
2021-03-15 17:51:43 +0100

    work in progress

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -45,7 +45,7 @@ class RidesController < ApplicationController
 
   def destroy
     @ride.destroy
-    redirect_to account_rides_path, notice: 'Your ride was successfully deleted.'
+    redirect_to ride_path(), notice: 'Your ride was successfully deleted.'
   end
 
   private
