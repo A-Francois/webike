ea418fa617dc51bfc611c2bf31884d4051523330
Alexis Francois <francoisalexis.pro@gmail.com>
2021-03-18 15:00:24 +0100

    ok

--- app/channels/channel_ride_channel.rb
+++ app/channels/channel_ride_channel.rb
@@ -2,10 +2,8 @@ class ChannelRideChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     consumer.subscriptions.create({ channel: 'ChatroomChannel', id: '42' }, {
-  received(data) {
-    // called when data is broadcast in the cable
-  }
-});
+    #called when data is broadcast in the cable
+    });
   end
 
   def unsubscribed
