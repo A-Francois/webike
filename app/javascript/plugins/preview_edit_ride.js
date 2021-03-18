823ea72ed4dccf82a21cf2259d75441db092c7fe
nicolas duchamp <nicolas.duchamp60@gmail.com>
2021-03-18 17:52:36 +0100

    update corrected

--- app/javascript/plugins/preview_edit_ride.js
+++ app/javascript/plugins/preview_edit_ride.js
@@ -1,5 +1,7 @@
 const previewEditRide = (() => {
-  const previewButton = document.getElementById("preview-button");
+  const previewButton = document.getElementById("preview-ride-btn");
+  const validateButton = document.getElementById("update-ride-btn");
+  const form = document.querySelector(".edit_ride");
   const rideTitle = document.getElementById("ride_title");
   const rideCityDeparture = document.getElementById("ride_city_departure");
   const rideCityArrival = document.getElementById("ride_city_arrival");
@@ -10,6 +12,10 @@ const previewEditRide = (() => {
     previewButton.addEventListener('click', (event) => {
       event.preventDefault();
       document.location.href = window.location.pathname + `?title=${rideTitle.value}&city_departure=${rideCityDeparture.value}&city_arrival=${rideCityArrival.value}&departure_date=${rideDepartureDate.value}&arrival_date=${rideArrivalDate.value}&ride_description=${rideRideDescription.value}`
+    })
+    validateButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      form.submit();
     })
   }
 })
